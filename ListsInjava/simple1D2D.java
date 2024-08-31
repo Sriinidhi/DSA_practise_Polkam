edc8571ecc69d88ae430b06dfcf14a2cfd8f393d
Polkam Srinidhi <102644860+Sriinidhi@users.noreply.github.com>
2024-08-31 13:35:24 +0530

    Update simple1D2D.java
    try running on online compiler for better understanding and clarity on approach through comments in code.

--- ListsInjava/simple1D2D.java
+++ ListsInjava/simple1D2D.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class simple1d2d{
+class simple1d{
     public static void main(String args[])
     {
         List<Integer> list1d1 = new ArrayList<>();
@@ -9,17 +9,17 @@ class simple1d2d{
         list1d2.add(2);
         
 
-        System.out.println(list1d1);
-        System.out.println(list1d2);
+        System.out.println(list1d1);//[1]
+        System.out.println(list1d2);//[1,2]
 
         List<List<Integer>> list2d = new ArrayList<>();
-        System.out.println(list2d);
+        System.out.println(list2d);//[]
         list2d.add(list1d1);
         list2d.add(list1d2);
-        System.out.println(list2d);
+        System.out.println(list2d);//[[1],[1,2]]
 
         List<List<Integer>> list2d2 = new ArrayList<>(2);
-        System.out.println(list2d2);
+        System.out.println(list2d2);//[]
 
         for(int i=1; i<=3 ; i++)
         {
@@ -32,14 +32,39 @@ class simple1d2d{
         }
 
         System.out.println(list2d2);
+
+List<List<Integer>> list2d33 = new ArrayList<>();
+
+for (int i = 0; i <= 1; i++) {
+    list2d33.add(new ArrayList<>());  // Add an empty inner list directly to list2d3
+    for (int j = 0; j <= i; j++) {
+        list2d33.get(i).add(j);  // Add elements to the current inner list directly
+}
+	}
+	System.out.println(list2d33);//[[0],[0,1]]
+
+List<List<Integer>> list2d3 = new ArrayList<>();
+        System.out.println(list2d3);//[]
+
+        for (int i = 1; i <= 3; i++) {
+    list2d3.add(new ArrayList<>(List.of(i)));
+}
+
+        System.out.println(list2d3);
     }
 }
 
 /* output
-[1]
-[1, 2]
-[]
-[[1], [1, 2]]
-[]
-[[1], [1, 2], [1, 2, 3]]
+[1] -> list1d1
+[1, 2] -> list1d2
+[] -> list2d before
+[[1], [1, 2]] -> list2d after
+[] -> list2d2 before
+[[1], [1, 2], [1, 2, 3]] -> list2d2 after
+[[0],[0,1]] -> list2d33 created without innerList
+[] -> list2d3 before
+[[1], [2], [3]] -> list2d3 after
+
+Try running on online compiler for better understanding...
+happy Learning
 */
